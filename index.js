5aeff9c9dde6a0929f21611ca1baefd94b9464e6
alu@ubuntu20-ZeDHP <dev.bhorta@gmail.com>
2020-09-21 00:09:07 +0600

    add cors

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-
+const cors = require('cors')
+ 
 const app = express();
 const PORT = 8888;
 
 const SECRET = "super-secret"; // to be replaced with secret in .env var / privateKey
 
+app.use(cors());
+
+
 app.get("/api/user", (req, res) => {
-  console.log("req.query.jwt", req.query.jwt);
   jwt.verify(req.query.jwt, SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({
